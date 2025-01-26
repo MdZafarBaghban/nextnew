b562fb9a97ffcd0afd27880d91d2a13662f1f30b
MdZafarBaghban <108886089+MdZafarBaghban@users.noreply.github.com>
2025-01-27 00:01:50 +0530

    Update page.js
    new resume updated
    
    Signed-off-by: MdZafarBaghban <108886089+MdZafarBaghban@users.noreply.github.com>

--- src/app/myself/page.js
+++ src/app/myself/page.js
@@ -4,7 +4,7 @@ import styles from './myself.module.css';
 const myself = () => {
   return (
     <div className={styles.container}>
-        <Image src="/merncv.jpg" alt="my logo image" width={500} height={700} className={styles.img} />
+        <Image src="/resume.jpg" alt="my logo image" width={500} height={700} className={styles.img} />
     </div>
   )
 }
